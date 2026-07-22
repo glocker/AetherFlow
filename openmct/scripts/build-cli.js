bc327d9305e2ab71442c9a3aab7c95ae0888dc29
glocker <georgeglocker@gmail.com>
2026-07-22 18:00:55 +0000

    Add successfull log to console

--- openmct/scripts/build-cli.js
+++ openmct/scripts/build-cli.js
@@ -1,6 +1,10 @@
 import { buildDashboard } from './build.js';
 
-buildDashboard().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+buildDashboard()
+  .then(() => {
+    console.log('Dashboard build completed');
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
